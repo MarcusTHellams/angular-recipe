173ccfbf0607ef22465714867772d9dff0f616f0
Marcus <mhellams@hotmail.com>
2016-10-09 22:59:33 -0400

    Custom Directive

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+  selector: '[rbHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+      this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green')
+   }
+
+}
