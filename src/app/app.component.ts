34c910308fa2c9ab9567bd16def3fe0eaf5cc95b
Marcus <mhellams@hotmail.com>
2016-10-14 01:50:21 -0400

    Course Project - Creating the Shopping List Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RecipeService } from './recipes';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  providers: [RecipeService]
+  providers: [
+    RecipeService,
+    ShoppingListService
+  ]
 })
 export class AppComponent {
 }
