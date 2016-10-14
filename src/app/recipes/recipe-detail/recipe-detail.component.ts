1254d42f95f67aa376209f7422c74bac5a0b64c2
Marcus <mhellams@hotmail.com>
2016-10-14 02:53:32 -0400

    Course Project - Cross-Service Communication

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipes';
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
-@Input() selectedRecipe: Recipe;
-  constructor() { }
+  @Input() selectedRecipe: Recipe;
+
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
   }
+
+  onAddToShoppingList() {
+    console.log(this.selectedRecipe.ingredients);
+    this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Recipe} from './recipe';
+import { Recipe } from './recipe';
+import { Ingredient } from './../shared';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very Tasty', 'http://lorempixel.com/100/100/food/1', []),
+    new Recipe('Schnitzel', 'Very Tasty', 'http://lorempixel.com/100/100/food/1', [new Ingredient('French Fries', 2), new Ingredient('Pork Meat', 1)]),
     new Recipe('Summer Salad', 'Okay Tasty', 'http://lorempixel.com/100/100/food/2', [])
   ];
   constructor() { }
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes;
   }
 
