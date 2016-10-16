03f20a0b9dbb6fdb2a44206fe509bc29babd7ab8
Marcus <mhellams@hotmail.com>
2016-10-16 19:48:54 -0400

    Recipe Form Preparaton Fetching the Active Recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeService } from './../recipe.service';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styles: []
 })
-export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private recipeIndex: number;
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew = true;
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {
+          this.recipeIndex = +params['id'];
+          isNew = false;
+        } else {
+          isNew = true;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
