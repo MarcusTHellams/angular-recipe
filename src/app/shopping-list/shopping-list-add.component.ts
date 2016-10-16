49b83dac47045256a0766aa067aae9a9997cc59d
Marcus <mhellams@hotmail.com>
2016-10-16 18:19:52 -0400

    Displaying Data with ngModel

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -9,21 +9,22 @@ import { ShoppingListService } from './shopping-list.service';
 export class ShoppingListAddComponent implements OnChanges {
   isAdd = true;
   @Input() item: Ingredient;
-  constructor(private sls: ShoppingListService) {  }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnChanges(changes) {
-    if(changes.item.currentValue === null){
+    if (changes.item.currentValue === null) {
       this.isAdd = true;
+      this.item = { name: null, amount: null }
     } else {
       this.isAdd = false;
     }
   }
-  onSubmit(ingredient: Ingredient){
-    if(!this.isAdd){
- 
+  onSubmit(ingredient: Ingredient) {
+    if (!this.isAdd) {
+
     } else {
       this.item = new Ingredient(ingredient.name, ingredient.amount)
       this.sls.addItem(this.item);
     }
-   }
+  }
 }
